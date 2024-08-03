bd05a87fbfd51ba79926045929fb0e4d1153a499
HugeKyung <kiki9510@gmail.com>
2024-08-03 17:34:45 +0900

    initial setting with spring-boot

---
+++ src/main/java/com/heyblossom/sb_for_vsc/SbForVscApplication.java
@@ -0,0 +1,14 @@
+package com.heyblossom.sb_for_vsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbForVscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbForVscApplication.class, args);
+		System.out.println("Hello, World!");
+	}
+
+}
